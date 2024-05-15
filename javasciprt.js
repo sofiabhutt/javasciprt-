34eebb75381fc728483bd107f4087c995eeca388
sofiabhutt <162460383+sofiabhutt@users.noreply.github.com>
2024-05-15 15:54:18 +0500

    Update javasciprt.js

---
+++ javasciprt.js
@@ -0,0 +1,40 @@
+let age = +prompt(" Enter your are age"); 
+if(age>18){  
+console.log("your are eligible for vote") 
+} 
+else{
+console.log("your are not eligible for vote")
+} 
+let studentgrade = +prompt("enter your test score");
+if(studentgrade>=60){
+console.log("you passed the test")
+} 
+else{
+    console.log("you failed the test")
+} 
+let raining = true;
+if(raining ==true){
+console.log("Remember to bring an umbrella") 
+} 
+else{
+console.log("enjoy the sunny weather")
+}
+let number = +prompt("Enter the number")
+if(number> 0){
+console.log(" the number is positive");
+} 
+else if(number> 0) 
+console.log("the number is negative")  
+else if(number> 0){
+console.log("the number zero")
+} 
+else{
+console.log("all number are integer")
+} 
+let score = +prompt("Enter your score") 
+if(score >= 80 && score <= 60){
+    console.log("you are passed")
+} 
+else{
+    console.log("you are failed")
+}
